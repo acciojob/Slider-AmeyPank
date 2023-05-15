47f3b32c54ae9192e471ec1cac84ef073c3e9587
AmeyPank <pankantiameya@gmail.com>
2023-05-16 01:58:38 +0530

    Package for submission

--- parser.js
+++ parser.js
@@ -7,3 +7,11 @@ const options = {compact: true, ignoreComment: true, spaces: 4};
 const result = parser.xml2json(xml, options)
 
 console.log(result);
+
+
+
+// "scripts": {
+//     "test": "NODE_OPTIONS=--openssl-legacy-provider jest --no-color 2>output.txt",
+//     "start": "NODE_OPTIONS=--openssl-legacy-provider webpack-dev-server --mode development --watch --open --hot",
+//     "build": "NODE_OPTIONS=--openssl-legacy-provider webpack --mode production"
+//   },
